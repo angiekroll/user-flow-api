f454e52704d7e9b775a5f52a2e8aa1903e55fb5a
AngelaCastillo <angiekroll@gmail.com>
2023-03-09 18:25:39 -0500

    rename DTO

--- src/main/java/com/neoris/user_flow_api/exception/HandlerExceptions.java
+++ src/main/java/com/neoris/user_flow_api/exception/HandlerExceptions.java
@@ -1,6 +1,6 @@
 package com.neoris.user_flow_api.exception;
 
-import com.neoris.user_flow_api.dto.NotificationDto;
+import com.neoris.user_flow_api.dto.NotificationDTO;
 import java.time.Instant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +15,7 @@ public class HandlerExceptions {
   public ResponseEntity<Object> handlerCarPoolingException(UserFlowException ex) {
     log.error("Exception: [{}]", ex.getErrorCode().getDescription(), ex);
     return new ResponseEntity<>(
-        NotificationDto.builder().message(ex.getErrorCode().getDescription())
+        NotificationDTO.builder().message(ex.getErrorCode().getDescription())
             .status(ex.getErrorCode().getHttpStatus().value())
             .error(ex.getErrorCode().getHttpStatus().getReasonPhrase())
             .timestamp(Instant.now())
